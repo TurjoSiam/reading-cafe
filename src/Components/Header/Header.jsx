d6f559c7e6ca63f8ca5fc602710f4daac8a6bc13
TurjoSiam <t.siam24@gmail.com>
2024-10-24 12:43:41 +0600

    header section added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,10 @@
+import profile from '../../assets/images/profile.png'
+
 const Header = () => {
     return (
-        <div>
-            
+        <div className='w-11/12 mx-auto my-5 border-b-2 flex items-center justify-between'>
+            <h2 className='text-3xl font-bold'>Knowledge Cafe</h2>
+            <img src={profile} alt="" />
         </div>
     );
 };
